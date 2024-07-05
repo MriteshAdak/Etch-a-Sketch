7acbe86f8510645ce0654c8e197e39a290aab506
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-07-06 00:24:14 +0530

    added the logic to sketch, clear and change grid size

--- script.js
+++ script.js
@@ -1,26 +1,63 @@
 // implemeting script for Etch-a-Sketch
 
-let rows = 16;
-let columns = 16;
+let side = 16;
 const gridContainer = document.querySelector("#gridContainer");
 
-function makeGrid(rows, columns)
+makeGrid(side);
+
+const gridSize = document.querySelector("#gridSize");
+const sketch = document.querySelector("#sketch");
+
+gridSize.addEventListener("click", () =>
+    {
+        while (gridContainer.firstChild)
+            gridContainer.lastChild.remove();
+        
+        do
+        {
+            side = parseInt(prompt("Enter the grid size of your choice (<100): "));
+        } while(side > 100);
+        
+        makeGrid(side);
+    });
+
+sketch.addEventListener("click", () => 
+    {
+        const grids = document.querySelectorAll(".grids");
+        for (const grid of grids)
+            grid.style.backgroundColor = "white";
+    });
+
+function makeGrid(side)
 {
-    for (let i = 0; i < rows; i++)
+    for (let i = 0; i < side; i++)
     {
         const gridRows = document.createElement("div");
         gridRows.style.display = "flex";
         gridRows.style.flexGrow = "1";
         gridContainer.appendChild(gridRows);
 
-        for (let j = 0; j < columns; j++)
+        for (let j = 0; j < side; j++)
         {
             const gridColumns = document.createElement("div");
             gridColumns.style.flexGrow = "1";
-            // gridColumns.style.border = "solid 1px black";
+            gridColumns.className = "grids";
             gridRows.appendChild(gridColumns);
+            gridColumns.addEventListener("mouseover", (e) =>
+                {
+                    e.target.style.backgroundColor = randomColour();
+                });
         }
     }
 }
 
-makeGrid(rows, columns);
+function randomColour()
+{
+    const keys = "0123456789ABCDEF";
+    let colour = "#";
+    
+    for (let i = 0; i < 6; i++)
+        colour += keys[Math.floor(Math.random() * 16)];
+
+    return colour;
+}
